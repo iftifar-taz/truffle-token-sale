42a87150e9fa66a85280b3d7da16252e0996ea58
Iftifar Taz <iftifar.taz@northsouth.edu>
2019-04-15 21:07:01 +0600

    delegated transfer

--- contracts/ItToken.sol
+++ contracts/ItToken.sol
@@ -6,11 +6,18 @@ contract ItToken {
     string public standard;
     uint256 public totalSupply;
 
-    mapping(address=>uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint _value
+    );
+
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
         uint _value
     );
 
@@ -27,6 +34,22 @@ contract ItToken {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approve(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
--- test/ItToken.test..js
+++ test/ItToken.test..js
@@ -15,47 +15,90 @@ contract('ItToken', (accounts) => {
 
     it('initializes contract with correct values', async () => {
         const name = await this.itToken.name();
-        const symble = await this.itToken.symble();
         assert.equal(name, 'It Token');
+        const symble = await this.itToken.symble();
         assert.equal(symble, 'ITT');
-    });
-
-    it('sets total supply', async () => {
         const totalSupply = await this.itToken.totalSupply();
         assert.equal(totalSupply.toNumber(), 1000000000);
-    });
-
-    it('sets admin balance', async () => {
         const balanceOfAdmin = await this.itToken.balanceOf(accounts[0]);
         assert.equal(balanceOfAdmin.toNumber(), 1000000000);
     });
 
-    it('transfer token', async () => {
+    it('transfer token ownership', async () => {
         try {
             await this.itToken.transfer.call(accounts[1], 10000000000);
             // .call will not create a transection and return the specified return values
         } catch (err) {
             assert(err.message.indexOf('revert') >= 0);
         }
-        await this.itToken.transfer(accounts[1], 100, {from: accounts[0]});
+
+        let returnValue = await this.itToken.transfer.call(accounts[1], 100);
+        assert.equal(returnValue, true);
+
+        let receipt = await this.itToken.transfer(accounts[1], 100, { from: accounts[0] });
         // not using .call will create a transaction and return the receipt hash
+
+        assert.equal(receipt.logs.length, 1);
+        assert.equal(receipt.logs[0].event, 'Transfer');
+        assert.equal(receipt.logs[0].args._from, accounts[0]);
+        assert.equal(receipt.logs[0].args._to, accounts[1]);
+        assert.equal(receipt.logs[0].args._value, 100);
+
         const balance0 = await this.itToken.balanceOf(accounts[0]);
         assert.equal(balance0.toNumber(), 999999900);
         const balance1 = await this.itToken.balanceOf(accounts[1]);
         assert.equal(balance1.toNumber(), 100);
     });
 
-    it('transfer token event', async () => {
-        let receipt = await this.itToken.transfer(accounts[1], 100, {from: accounts[0]});
+    it('approves tokens for delegated transfers', async () => {
+        let returnValue = await this.itToken.approve.call(accounts[1], 100);
+        assert.equal(returnValue, true);
+
+        const receipt = await this.itToken.approve(accounts[1], 100, { from: accounts[0] });
         assert.equal(receipt.logs.length, 1);
-        assert.equal(receipt.logs[0].event, 'Transfer');
-        assert.equal(receipt.logs[0].args._from, accounts[0]);
-        assert.equal(receipt.logs[0].args._to, accounts[1]);
+        assert.equal(receipt.logs[0].event, 'Approve');
+        assert.equal(receipt.logs[0].args._owner, accounts[0]);
+        assert.equal(receipt.logs[0].args._spender, accounts[1]);
         assert.equal(receipt.logs[0].args._value, 100);
+
+        const allowance = await this.itToken.allowance(accounts[0], accounts[1]);
+        assert.equal(allowance.toNumber(), 100);
     });
 
-    it('transfer token return value', async () => {
-        let returnValue = await this.itToken.transfer.call(accounts[1], 100);
-        assert.equal(returnValue, true);
+    it('handles delegated token transfer', async () => {
+        fromAcc = accounts[2];
+        toAcc = accounts[3];
+        spendingAcc = accounts[4];
+        await this.itToken.transfer(fromAcc, 2000, { from: accounts[0] });
+        await this.itToken.approve(spendingAcc, 100, { from: fromAcc });
+        try {
+            await this.itToken.transferFrom(fromAcc, toAcc, 9999, { from: spendingAcc });
+        } catch (err) {
+            assert(err.message.indexOf('revert') >= 0);
+        }
+        try {
+            await this.itToken.transferFrom(fromAcc, toAcc, 999, { from: spendingAcc });
+        } catch (err) {
+            assert(err.message.indexOf('revert') >= 0);
+        }
+        const returnVal = await this.itToken.transferFrom.call(fromAcc, toAcc, 100, { from: spendingAcc });
+        assert.equal(returnVal, true);
+
+        const receipt = await this.itToken.transferFrom(fromAcc, toAcc, 100, { from: spendingAcc });
+        assert.equal(receipt.logs.length, 1);
+        assert.equal(receipt.logs[0].event, 'Transfer');
+        assert.equal(receipt.logs[0].args._from, fromAcc);
+        assert.equal(receipt.logs[0].args._to, toAcc);
+        assert.equal(receipt.logs[0].args._value, 100);
+
+        const balance2 = await this.itToken.balanceOf(fromAcc);
+        assert.equal(balance2.toNumber(), 1900);
+        const balance3 = await this.itToken.balanceOf(toAcc);
+        assert.equal(balance3.toNumber(), 100);
+        const balance4 = await this.itToken.balanceOf(spendingAcc);
+        assert.equal(balance4.toNumber(), 0);
+
+        const allowance = await this.itToken.allowance(fromAcc, spendingAcc);
+        assert.equal(allowance.toNumber(), 0);
     });
-})
+});
